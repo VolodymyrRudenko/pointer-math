6346815a92420fad712ed768018d6f112fb931c9
VolodymyrRudenko <59016863+VolodymyrRudenko@users.noreply.github.com>
2020-01-16 00:39:22 +0300

    add math

---
+++ Math.java
@@ -0,0 +1,34 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+public class Math {
+    static final double SpeedLIGHT = 2.99792458E8D;
+    static final double e = 2.718281D;
+    static final double Y = 0.577D;
+
+    public Math() {
+    }
+
+    static int KvadratTwoNumber(int a, int b) {
+        int i = a * b;
+        return i;
+    }
+
+    static int SummaTwoNumber(int a, int b) {
+        int i = a + b;
+        return i;
+    }
+
+    static int DivideTwoNumber(int a, int b) {
+        int i = a / b;
+        return i;
+    }
+
+    static int MinusTwoNumber(int a, int b) {
+        int i = a - b;
+        return i;
+    }
+}
+//
